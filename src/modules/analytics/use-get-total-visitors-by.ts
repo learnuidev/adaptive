de0cabe739fee6afcc21a931135d264940681f56
learnuidev <learnuidev@gmail.com>
2025-09-29 10:41:08 -0400

    wip

---
+++ src/modules/analytics/use-get-total-visitors-by.ts
@@ -0,0 +1,64 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { appConfig } from "@/lib/app-config";
+import { fetchWithToken } from "@/lib/aws-smplify/fetch-with-token";
+import { useQuery } from "@tanstack/react-query";
+import { FilterPeriod } from "./analytics.types";
+
+export type GetVisitorsByResponse = { name: string; visitors: string }[];
+
+export interface GetTotalVisitorsByParams {
+  websiteId: string;
+  period: FilterPeriod;
+  from?: Date;
+  to?: Date;
+  groupBy: "country" | "region" | "city" | "browser_name" | "os_name";
+}
+
+async function getTotalVisitorsBy({
+  websiteId,
+  period,
+  groupBy,
+  from,
+  to,
+}: GetTotalVisitorsByParams): Promise<GetVisitorsByResponse> {
+  const res = await fetchWithToken(
+    `${appConfig.apiUrl}/v1/analytics/get-total-visitors-by`,
+    {
+      method: "POST",
+      body: JSON.stringify({ groupBy, from, to }),
+    }
+  );
+
+  if (!res.ok) {
+    throw Error(`Something went wrong`);
+  }
+
+  const respRaw = (await res.json()) as GetVisitorsByResponse;
+
+  return respRaw;
+}
+const getSummaryQueryKey = "get-summary";
+
+export const useGetTotalVisitorsByQuery = ({
+  websiteId,
+  period,
+  groupBy,
+}: GetTotalVisitorsByParams) => {
+  return useQuery({
+    queryKey: [getSummaryQueryKey, websiteId, period, groupBy],
+    queryFn: async () => {
+      const response = await getTotalVisitorsBy({
+        websiteId,
+        period,
+        groupBy,
+      });
+      return response;
+    },
+    // refetchInterval: 5000,
+
+    refetchOnMount: false,
+    refetchOnReconnect: false,
+    refetchOnWindowFocus: false,
+    refetchIntervalInBackground: false,
+  });
+};
