a7d97acfc5ee24ad99dcb02331d68af4769306f8
learnuidev <learnuidev@gmail.com>
2025-10-02 17:03:08 -0400

    added list cohort users

---
+++ src/modules/cohort/list-cohort-users-query.ts
@@ -0,0 +1,37 @@
+import { useQuery } from "@tanstack/react-query";
+
+import { appConfig } from "@/lib/app-config";
+import { fetchWithToken } from "@/lib/aws-smplify/fetch-with-token";
+
+export interface CohortUser {
+  id: string;
+  email?: string;
+  createdAt: string;
+}
+
+const listCohortUsers = async (cohortId: string): Promise<CohortUser[]> => {
+  const res = await fetchWithToken(
+    `${appConfig.apiUrl}/v1/analytics/list-cohort-users`,
+    {
+      method: "POST",
+      body: JSON.stringify({ cohortId }),
+    }
+  );
+
+  if (!res.ok) {
+    throw Error(`Something went wrong`);
+  }
+
+  const resp = await res.json();
+  return resp as CohortUser[];
+};
+
+export const listCohortUsersQueryKey = "list-cohort-users";
+
+export function useListCohortUsersQuery(cohortId: string) {
+  return useQuery({
+    queryKey: [listCohortUsersQueryKey, cohortId],
+    queryFn: () => listCohortUsers(cohortId),
+    enabled: !!cohortId,
+  });
+}
